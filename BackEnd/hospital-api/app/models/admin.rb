5e3d04877d5be5100c8cd843b8a3a07f14fe944d
Edvard Avagyan <=>
2018-12-10 19:16:06 +0400

    feat: add validations for admin

--- BackEnd/hospital-api/app/models/admin.rb
+++ BackEnd/hospital-api/app/models/admin.rb
@@ -4,4 +4,6 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :fname, :lname, presence: true
 end
