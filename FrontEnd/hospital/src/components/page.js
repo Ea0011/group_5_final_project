c72e0c8ddcc384d81581d012dea6e077d7a26e6c
Edvard Avagyan <=>
2018-12-15 00:25:06 +0400

    feat: add main page and patient list components

---
+++ FrontEnd/hospital/src/components/page.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import Calendar from './calendar';
+import * as doctorActions from '../actions/doctors';
+import * as appointmentsActions from '../actions/appointments';
+import * as proceduresActions from '../actions/procedures';
+import * as userActions from '../actions/user';
+import * as patientActions from '../actions/patients';
+import { connect } from 'react-redux';
+import Patients from './patients';
+import Grid from '@material-ui/core/Grid';
+
+class Page extends React.PureComponent {
+  state = {
+    loading: true
+  }
+
+  async componentDidMount() {
+    try {
+      const headers = new Headers();
+      headers.append("Authorization", window.localStorage.getItem("Authorization"));
+      const requests = await Promise.all([
+        fetch("http://localhost:3000/doctors", { headers }),
+        fetch("http://localhost:3000/appointments", { headers }),
+        fetch("http://localhost:3000/procedures", { headers }),
+        fetch("http://localhost:3000/patients", { headers })
+      ]);
+
+      const [ doctors, appointments, procedures, patients ] = await Promise.all(requests.map(req => req.json()));
+      doctors.forEach(doc => { this.props.addDoctor(doc) });
+      appointments.forEach(app => { this.props.addAppointment(app) });
+      procedures.forEach(proc => { this.props.addProcedure(proc) });
+      patients.forEach(patient => { this.props.addPatient(patient) });
+      this.setState({ loading: false });
+    } catch(e) {
+      console.error(e);
+    }
+  }
+
+  render() {
+    return(
+      this.state.loading ? null :
+      <React.Fragment>
+        <Calendar style={{ height: '30%' }}/>
+        <Grid container direction="row" style={{ height: '70%' }}>
+          <Grid container style={{width: '50%'}}>
+            <Grid item>
+              <Patients />
+            </Grid>
+          </Grid>
+          <Grid container style={{width: '50%'}}>
+            <Grid item>
+              <Patients />
+            </Grid>
+          </Grid>
+        </Grid>
+      </React.Fragment>
+    )
+  }
+}
+
+const mapDispatchProps = dispatch => {
+  return {
+    addDoctor: (doctor) => { dispatch(doctorActions.addDoctor(doctor)) },
+    addAppointment: (appointment) => { dispatch(appointmentsActions.addAppointment(appointment)) },
+    addProcedure: (procedure) => { dispatch(proceduresActions.addProcedure(procedure)) },
+    addPatient: (patient) => { dispatch(patientActions.addPatient(patient)) },
+    setUser: (user) => { dispatch(userActions.setUser(user)) }
+  }
+}
+
+export default connect(null, mapDispatchProps)(Page);
---
+++ FrontEnd/hospital/src/components/patient_list.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { connect } from 'react-redux';
+import * as selectors from '../selectors/selectors';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import { Link } from 'react-router-dom';
+import { IconButton } from '@material-ui/core';
+import Delete from '@material-ui/icons/Delete';
+import Edit from '@material-ui/icons/Edit';
+
+const PatientList = ({ getPatients, removePatient }) => {
+  return(
+    <React.Fragment>
+      <Typography variant="h6">Patients</Typography>
+      <Grid container direction="column">
+        {getPatients().map(patient => (
+          <Grid item key={patient.id}>
+            <List>
+              <ListItem>
+                <ListItemText
+                  primary={patient.fname + " " + patient.lname}
+                  secondary={patient.pnumber}
+                />
+              </ListItem>
+              <ListItemSecondaryAction style={{marginRight: -32}}>
+                <IconButton>
+                  <Delete onClick={(e) => { removePatient(patient.id) }}/>
+                </IconButton>
+              </ListItemSecondaryAction>
+              <ListItemSecondaryAction style={{marginRight: -84}}>
+                <Link to={`/patients/${patient.id}`} style={{textDecoration: 'none'}}>
+                  <IconButton>
+                    <Edit />
+                  </IconButton>
+                </Link>
+              </ListItemSecondaryAction>
+            </List>
+          </Grid>
+        ))}
+      </Grid>
+    </React.Fragment>
+  )
+}
+
+const mapStateToProps = state => ({ getPatients: () => selectors.getPatients(state) });
+const mapDispatchToProps = dispatch => ({
+  removePatient: async(id) => {
+    try {
+      const headers = new Headers();
+      headers.append("Authorization", window.localStorage.getItem("Authorization"));
+      const params = { method: 'DELETE', headers }
+      const deleteRequest = await fetch(`http://localhost:3000/patients/${id}`, params);
+      const response = await deleteRequest.json();
+      console.log(response);
+    } catch(e) {
+      console.error(e);
+    }
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(PatientList);
