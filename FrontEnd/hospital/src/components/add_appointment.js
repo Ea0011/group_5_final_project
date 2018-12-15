e0fc4ae863ee884be14a11a6b76e8065799c37ff
Edvard Avagyan <=>
2018-12-16 01:54:32 +0400

    feat: add appointment scheduling component

---
+++ FrontEnd/hospital/src/components/add_appointment.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Grid, Typography, Select, MenuItem } from '@material-ui/core';
+import * as selectors from '../selectors/selectors';
+import * as appointmentActions from '../actions/appointments';
+import Button from '@material-ui/core/Button';
+
+class AddAppointment extends React.PureComponent {
+  state = {
+    patient: this.props.getPatients()[0].id,
+    procedure: this.props.getProcedures()[0].id,
+  }
+
+  handleSubmit = async() => {
+    try {
+      const headers = new Headers();
+      headers.append("Authorization", window.localStorage.getItem("Authorization"));
+      const formData = new FormData();
+      const { procedure, patient } = this.state;
+      const { startDate, doctor } = this.props;
+      formData.append("patient_id", patient);
+      formData.append("procedure_id", procedure);
+      formData.append("doctor_id", doctor.id);
+      formData.append("start_date", startDate);
+      formData.append("end_date", new Date(startDate.getTime() + (this.props.getProcedure(procedure).duration * 1000 * 60 * 60)));
+      const params = { method: 'POST', headers, body: formData };
+      const addRequest = await fetch("http://localhost:3000/appointments", params);
+      const response = await addRequest.json();
+      if (addRequest.ok) {
+        this.props.addAppointment(response);
+      } else {
+        alert("Something went wrong, check times");
+      }
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return(
+      <React.Fragment>
+        <Grid container direction="column" alignItems="center">
+          <Typography variant="h6">{this.props.doctor.fname + " " + this.props.doctor.lname}</Typography>
+          <h6>select procedure</h6>
+          <Select value={this.state.procedure} style={{width: '80%'}} onChange={(e) => { console.log(e); this.setState({ procedure: e.target.value }) }}>
+            {this.props.getProcedures().map(proc => <MenuItem key={proc.id} value={proc.id}>{proc.name}</MenuItem>)}
+          </Select>
+          <h6>select patient</h6>
+          <Select value={this.state.patient} style={{width: '80%'}} onChange={(e) => { this.setState({ patient: e.target.value }) }}>
+            {this.props.getPatients().map(patient => <MenuItem key={patient.id} value={patient.id}>{patient.fname +
+             " " + patient.lname}</MenuItem>)}
+          </Select>
+          <h6>Start Date: { this.props.startDate.toDateString() + " " + this.props.startDate.getHours() + " : " + this.props.startDate.getMinutes()}</h6>
+          <h6>Duration: {this.props.getProcedure(this.state.procedure).duration + " Hrs"}</h6>
+        </Grid>
+        <Button onClick={this.handleSubmit}>Submit</Button>
+      </React.Fragment>
+    )
+  }
+}
+
+const mapStateToProps = (state, { match }) => ({
+  doctor: state.doctors[match.params.specialist][match.params.id],
+  getProcedures: () => selectors.getProceduresBySpecialist(state, match.params.specialist),
+  getPatients: () => selectors.getPatients(state),
+  startDate: new Date(parseInt(match.params.startDate)),
+  getProcedure: (id) => selectors.getProcedureById(state, id)
+});
+
+const mapDispatchToProps = dispatch => ({
+  addAppointment: (appointment) => {dispatch(appointmentActions.addAppointment(appointment))}
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddAppointment);
